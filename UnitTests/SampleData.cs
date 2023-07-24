81290ed4e7d04373307416f27276df674e836900
Derek Gilbert <derek.gilbert@payfactors.com>
2023-07-23 23:59:24 -0400

    simple stock functions

---
+++ UnitTests/SampleData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class StockPrice
+{
+    public DateTime Date { get; set; }
+    public double OpenPrice { get; set; }
+    public double HighPrice { get; set; }
+    public double LowPrice { get; set; }
+    public double ClosePrice { get; set; }
+    public long Volume { get; set; }
+
+    public StockPrice(DateTime date, double openPrice, double highPrice, double lowPrice, double closePrice, long volume)
+    {
+        Date = date;
+        OpenPrice = openPrice;
+        HighPrice = highPrice;
+        LowPrice = lowPrice;
+        ClosePrice = closePrice;
+        Volume = volume;
+    }
+}
+
+public class SampleData
+{
+    public static List<StockPrice> GetSampleData()
+    {
+        List<StockPrice> stockPrices = new List<StockPrice>
+        {
+            // Sample data for a hypothetical stock
+            new StockPrice(new DateTime(2023, 7, 18), 100.0, 105.5, 98.5, 103.2, 50000),
+            new StockPrice(new DateTime(2023, 7, 19), 104.0, 110.2, 101.0, 109.8, 60000),
+            new StockPrice(new DateTime(2023, 7, 20), 110.0, 115.8, 106.5, 113.5, 70000),
+            new StockPrice(new DateTime(2023, 7, 21), 112.0, 118.5, 110.2, 116.7, 80000),
+            new StockPrice(new DateTime(2023, 7, 22), 115.0, 120.0, 112.8, 118.4, 90000)
+        };
+
+        return stockPrices;
+    }
+}
---
+++ UnitTests/StockTests.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleExpressionEngine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class StockTests
+    {
+        [TestMethod]
+        public void Functions()
+        {
+            var ctx = new MyFunctionContext();
+            //Assert.AreEqual(Parser.Parse("avgv(3)").Eval(ctx), 80000);
+            //Assert.AreEqual(Parser.Parse("avgv(3,2)").Eval(ctx), 60000);
+            Assert.AreEqual(Parser.Parse("maxc(5)").Eval(ctx), 118.4);
+            Assert.AreEqual(Parser.Parse("maxc(3,2)").Eval(ctx), 113.5);
+        }
+
+        [TestMethod]
+        public void FunctionsThatDontRequireArgs()
+        {
+            var ctx = new MyFunctionContext();
+            Assert.AreEqual(Parser.Parse("c()").Eval(ctx), 118.4);
+            Assert.AreEqual(Parser.Parse("c(5)").Eval(ctx), 113.5);
+        }
+    }
+
+    public class FunctionProperties
+    {
+        public int MinArgs { get; set; }
+        public int MaxArgs { get; set; }
+
+    }
+
+    public class Args
+    {
+        public int OffSet { get; set; }
+        public int Length { get; set; }
+    }
+
+
+    //https://medium.com/@toptensoftware/writing-a-simple-math-expression-engine-in-c-d414de18d4ce
+    class MyFunctionContext : IContext
+    {
+        private List<StockPrice> _data;
+        public MyFunctionContext()
+        {
+            _data = SampleData.GetSampleData();
+        }
+
+        public double ResolveVariable(string name)
+        {
+            throw new InvalidDataException($"Unknown variable: '{name}'");
+        }
+
+        public Args ConvertArgs(double[] args)
+        {
+            var offset = 0;
+            if (args.Length == 2)
+            {
+                //todo check type for cast?
+                offset = (int)args[1];
+            }
+
+            return new Args() { OffSet = offset, Length = (int)args[0] };
+        }
+
+        public double CallFunction(string name, double[] arguments)
+        {
+            if (name.Length == 4)
+            {
+                var ele = name[3];
+                var fun = name.Substring(0, 3);
+
+                Func<StockPrice, double> propertySelector = f => f.ClosePrice;
+                var args = ConvertArgs(arguments);
+
+                if (ele == 'o')
+                {
+                    propertySelector = f => f.OpenPrice;
+                }
+                else if (ele == 'h')
+                {
+                    propertySelector = f => f.HighPrice;
+                }
+                else if (ele == 'l')
+                {
+                    propertySelector = f => f.LowPrice;
+                }
+
+                if (fun == "avg")
+                {
+                    return Avg(args, propertySelector);
+                }
+                else if (fun == "max")
+                {
+                    return Max(args, propertySelector);
+                }
+                else if (fun == "min")
+                {
+                    return Min(args, propertySelector);
+                }
+            }
+
+            throw new InvalidDataException($"Unknown function: '{name}'");
+        }
+
+        private t Max<t>(Args args, Func<StockPrice, t> propertyDelegate)
+        {
+            var len = _data.Count - args.Length;
+            return _data.Skip(len).Take(args.Length - args.OffSet).Max(propertyDelegate);
+        }
+
+        private t Min<t>(Args args, Func<StockPrice, t> propertyDelegate)
+        {
+            var len = _data.Count - args.Length;
+            return _data.Skip(len).Take(args.Length - args.OffSet).Min(propertyDelegate);
+        }
+
+        private double Avg(Args args, Func<StockPrice, double> propertyDelegate)
+        {
+            var len = _data.Count - args.Length;
+            return _data.Skip(len).Take(args.Length - args.OffSet).Average(propertyDelegate);
+        }
+    }
+}
